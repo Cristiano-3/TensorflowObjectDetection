0be5e8d8942755778dff366d40441b682abff24c
Cristiano-3 <chunanluo@126.com>
2019-10-08 17:33:47 +0800

    add dataset & inputs define

--- detectron/models/retinanet.py
+++ detectron/models/retinanet.py
@@ -2,6 +2,8 @@
 import tensorflow as tf
 import numpy as np
 from configs import cfgs
+from utils.common import *
+
 
 class RetinaNet():
     def __init__(self, mode, trainset):
@@ -12,6 +14,11 @@ class RetinaNet():
         # get cfgs
         self.mode = mode
 
+        if mode == 'train':
+            self.train_generator = trainset['train_generator']
+            self.train_initializer, self.train_iterator = self.train_generator
+            
+
         # build network architecture
         self._define_inputs()
         self._build_detection_architecture()
@@ -20,12 +27,44 @@ class RetinaNet():
         self._init_session()
 
     def _init_session(self):
-        pass
+        self.sess = tf.Session()
+
+        # init global variables
+        self.sess.run(tf.global_variables_initializer())
+
+        # init data iterator
+        if self.mode == 'train':
+            if self.train_initializer is not None:
+                self.sess.run(self.train_initializer)
+        
 
     def _define_inputs(self):
+        """
+        shape/keep_aspect_ratio_resizer or fixed_shape_resizer
+        mean order, where to do minus mean, PIXEL_STD?
+        """
+        shape = [self.batch_size, None, None, 3]
+        mean = tf.convert_to_tensor([123.68, 116.779, 103.979], dtype=tf.float32)
+
+        if cfgs.data_format == 'channel_last':
+            mean = tf.reshape(mean, [1, 1, 1, 3])
+        else:
+            mean = tf.reshape(mean, [1, 3, 1, 1])
+
         # train mode
+        if self.mode == 'train':
+            self.images, self.ground_truth = self.train_iterator.get_next()
+            self.images.set_shape(shape)
+            self.images = self.images - mean
+        
         # test mode
-        pass
+        else:
+            self.images = tf.placeholder(tf.float32, shape, name='images')
+            self.images = self.images -mean
+            self.ground_truth = tf.placeholder(tf.float32, [self.batch_size, None, 5], name='labels')
+        
+        self.lr = tf.placeholder(dtype=tf.float32, shape=[], name='lr')
+
 
     def _build_detection_architecture(self):
         pass
