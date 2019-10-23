4930a0b184ee78209cb2b01f7b6dc6aaf54daf36
Cristiano-3 <chunanluo@126.com>
2019-10-23 19:05:49 +0800

    feed lr change to exp decay lr

--- tools/train_net.py
+++ tools/train_net.py
@@ -7,7 +7,7 @@ from datasets.voc_tfrecord_utils import get_generator
 from detectron.models.retinanet import RetinaNet
 
 
-def train(epochs, lr):
+def train(epochs):
     # prepare dataset
     data = [
         '../../Object-Detection-API-Tensorflow/data/train_00001-of-00010.tfrecord',
@@ -31,14 +31,9 @@ def train(epochs, lr):
     for i in range(epochs):
         print("-"*25, "epoch", i, "-"*25)
 
-        # reduce learning rate
-        if i % 50 == 0:
-            lr /= 10.0
-            print("reduce lr, lr=", lr)
-
         # train a epoch
-        retinanet.train_one_epoch(lr)
+        retinanet.train_one_epoch()
 
 
 if "__main__" == __name__:
-    train(cfgs.epochs, cfgs.lr)
+    train(cfgs.epochs)
